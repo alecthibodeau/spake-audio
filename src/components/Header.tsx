a566bfb35fda307f71be6c9a2237de378dc1e0c3
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-06-30 21:03:50 -0400

    Add Spake Audio logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
+/* Constants */
+import images from '../constants/images';
+
 function Header(): JSX.Element {
   return (
-    <header>Header</ header>
+    <header>
+      <img
+        className="spake-logo"
+        src={images.header.spakeLogo}
+        alt="Spake Audio logo" />
+    </ header>
   );
 }
 
---
+++ src/constants/images.ts
@@ -0,0 +1,12 @@
+/* Header */
+import spakeLogo from '../assets/images/header/spake-logo.svg';
+
+const header: { [key: string]: string } = {
+  spakeLogo
+};
+
+const images = {
+  header
+};
+
+export default images;
