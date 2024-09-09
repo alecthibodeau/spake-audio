158f3f9db2492e67db0710b305ff9a6339522eee
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-09-08 21:04:59 -0400

    Add image placeholder to continue laying out elements

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -4,6 +4,7 @@ import SectionProps from '../interfaces/SectionProps';
 function Section(props: SectionProps): JSX.Element {
   return (
     <section id={props.id}>
+      <div className="image-placeholder"></div>
       <h1>{props.heading[0].toUpperCase() + props.heading.slice(1)}</h1>
       <div>{props.description}</div>
     </section>
