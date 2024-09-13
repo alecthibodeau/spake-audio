b25e336b14d127525e586fb547752ae4801837a8
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-09-13 14:50:19 -0400

    Style large nav link to improve UX

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -7,6 +7,7 @@ import SectionProps from '../interfaces/SectionProps';
 import stringValues from '../constants/string-values'
 
 function Section(props: SectionProps): JSX.Element {
+  const { textAbout, textContact, textContactSpakeAudio } = stringValues;
   const thumbnails: string[] = Array(10).fill('thumbnail-image');
 
   function renderThumbnail(thumbnailImage: string, index: number): JSX.Element {
@@ -15,37 +16,46 @@ function Section(props: SectionProps): JSX.Element {
     );
   }
 
-  function renderContactTextLink(): JSX.Element {
+  function renderContactLink(isTextLink: boolean): JSX.Element {
     return (
-      <span>
-        &nbsp;
-        <NavLink
-          to={`/${stringValues.textContact}`}>
-          {stringValues.textContactSpakeAudio}
-        </NavLink>.
-      </span>
+      <NavLink
+        to={`/${textContact}`}
+        className={isTextLink ? 'section-text-link' : 'section-large-nav-link'}>
+        {textContactSpakeAudio}
+      </NavLink>
     );
   }
 
   return (
     <section>
       <div className="image-placeholder"></div>
-      <h1>
-        {props.heading[0].toUpperCase() + props.heading.slice(1)}
-      </h1>
-      <div>
-        <span>{props.description}</span>
+      <div className="section-content">
+        <h1>
+          {props.heading[0].toUpperCase() + props.heading.slice(1)}
+        </h1>
+        <div>
+          <span>{props.description}</span>
+          {
+            props.heading === textAbout ?
+            <span>
+              <span>&nbsp;</span>{renderContactLink(true)}<span>.</span>
+            </span> :
+            null
+          }
+        </div>
         {
-          props.heading === stringValues.textAbout ?
-          renderContactTextLink() :
+          props.heading === textAbout ?
+          <div>{thumbnails.map(renderThumbnail)}</div> :
+          null
+        }
+        {
+          props.heading !== textAbout && props.heading !== textContact ?
+          <div className="section-large-nav-link-wrapper">
+            {renderContactLink(false)}
+          </div> :
           null
         }
       </div>
-      {
-        props.heading === stringValues.textAbout ?
-        <div>{thumbnails.map(renderThumbnail)}</div> :
-        <button>{stringValues.textContactSpakeAudio}</button>
-      }
     </section>
   );
 }
