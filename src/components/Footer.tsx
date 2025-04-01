3af8de7772a41b2056d0d972401def17d9a61d0e
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-03-31 20:46:40 -0400

    feat: add scroll in future to display companies list

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,40 @@
+import React, { useEffect, useRef } from 'react';
+
 function Footer(): JSX.Element {
+  const scrollerRef = useRef<HTMLDivElement>(null);
+  const companies: string[] = [
+    'New York Times',
+    'Penguin Random House',
+    'HarperCollins Publishers',
+    'John Marshall Media',
+    'Dreamscape Media',
+    'Blackstone Publishing',
+    'Berklee Online'
+  ];
+
+  useEffect(() => {
+    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+      scrollerRef.current?.setAttribute('data-animated', 'true');
+    }
+  }, []);
+
+  function renderScrollItem(name: string, index: number): React.JSX.Element {
+    return (<div key={`${index}${name}`}>{name}</div>);
+  }
+
+  function renderHiddenScrollItem(name: string, index: number): React.JSX.Element {
+    return (<div key={`${index}Hidden${name}`} aria-hidden="true">{name}</div>);
+  }
+
   return (
     <footer>
-      <div>
+      <div className="scroller" ref={scrollerRef}>
+        <div className="scroller-inner">
+          {companies.map(renderScrollItem)}
+          {companies.map(renderHiddenScrollItem)}
+        </div>
+      </div>
+      <div className="footer-copyright">
         {`Copyright Spake Audio ${new Date().getFullYear()}`}
       </div>
     </footer>
