3ca793a3bb5b0146fcaeccf1a31d4bc76280511c
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-04-19 05:28:59 -0400

    feat: update header content to simplify navigation links

--- src/constants/string-values.ts
+++ src/constants/string-values.ts
@@ -25,12 +25,12 @@ const textText: string = 'text';
 const textTime: string = 'time';
 
 const emailServiceId: string = 'service_motlos2';
-const emailTemplateId: string = 'template_twmeleg';
+const emailTemplateId: string = 'template_va3kg47';
 const emailPublicKey: string = 'rv9Ct6p5U0kLM8po5';
 const inputFieldNames: string[] = ['name', 'email', 'phone', 'message'];
 const urlSendForm: string = 'https://api.emailjs.com/api/v1.0/email/send-form';
 
-const headerNavRoutes = [textRecording, textEditing, textConsulting, textAbout, textFAQ];
+const headerNavRoutes = [textRecording, textEditing, textConsulting];
 
 const companies: Company[] = [
   {
